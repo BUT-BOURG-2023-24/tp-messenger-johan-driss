73e4458c833ded7637314dd6e9c650d011893dfa
HMIMID DRISS p2106555 <driss.hmimid@etu.univ-lyon1.fr>
2023-10-17 11:39:32 +0200

    Init Conversation Model

--- src/database/Mongo/Models/ConversationModel.ts
+++ src/database/Mongo/Models/ConversationModel.ts
@@ -1,12 +1,36 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 import { MongooseID } from "../../../types";
 
 export interface IConversation extends Document {
-	//A COMPLETER
-}
+	title: string;
+	participants: Types.Array<Types.ObjectId | string>;
+	messages: Types.Array<Types.ObjectId | string>;
+	lastUpdate: Date;
+	seen: Map<number, number>;
+  }
 
 const conversationSchema: Schema<IConversation> = new Schema<IConversation>({
-	//A COMPLETER
+	title: {
+		type: String,
+		required: true,
+	  },
+	  participants: {
+		type: [{ type: Schema.Types.ObjectId, ref: "User" }], // Remplace "User" par le modèle réel de l'utilisateur si nécessaire.
+		required: true,
+	  },
+	  messages: {
+		type: [{ type: Schema.Types.ObjectId, ref: "Message" }], // Remplace "Message" par le modèle réel du message si nécessaire.
+		required: true,
+	  },
+	  lastUpdate: {
+		type: Date,
+		required: true,
+	  },
+	  seen: {
+		type: Map,
+		of: Number,
+		required: true,
+	  },
 });
 
 const ConversationModel = mongoose.model<IConversation>("Conversation", conversationSchema);
