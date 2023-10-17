d0940339902273b9eb9cafadcb85c5d40eebf579
Johan <johan.gagean@gmail.com>
2023-10-17 10:46:12 +0200

    Starting set up database

--- src/database/Mongo/Models/MessageModel.ts
+++ src/database/Mongo/Models/MessageModel.ts
@@ -2,11 +2,11 @@ import mongoose, { Schema, Document } from "mongoose";
 import { MongooseID } from "../../../types";
 
 export interface IMessage extends Document {
-	//A COMPLETER
+	
 }
 
 const MessageSchema: Schema<IMessage> = new Schema<IMessage>({
-	//A COMPLETER
+	
 });
 
 const MessageModel = mongoose.model<IMessage>("Message", MessageSchema);
