e38d77f9bbf89c7e0a09b2ed1cd80311cf6610f7
Johan <johan.gagean@gmail.com>
2023-11-07 15:32:57 +0100

    [UPDATE] - Little changes to ConversationModel.ts

--- src/database/Mongo/Models/ConversationModel.ts
+++ src/database/Mongo/Models/ConversationModel.ts
@@ -1,40 +1,21 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
-import { MongooseID } from "../../../types";
 
 export interface IConversation extends Document {
+	participants: string[];
+	messages: Types.ObjectId[];
 	title: string;
-	participants: Types.Array<Types.ObjectId | string>;
-	messages: Types.Array<Types.ObjectId | string>;
 	lastUpdate: Date;
-	seen: Map<number, number>;
-  }
+	seen: { [userId: string]: Types.ObjectId };
+}
 
 const conversationSchema: Schema<IConversation> = new Schema<IConversation>({
-	title: {
-		type: String,
-		required: true,
-	  },
-	  participants: {
-		type: [{ type: Schema.Types.ObjectId, ref: "User" }], // Remplace "User" par le modèle réel de l'utilisateur si nécessaire.
-		required: true,
-	  },
-	  messages: {
-		type: [{ type: Schema.Types.ObjectId, ref: "Message" }], // Remplace "Message" par le modèle réel du message si nécessaire.
-		required: true,
-	  },
-	  lastUpdate: {
-		type: Date,
-		required: true,
-	  },
-	  seen: {
-		type: Map,
-		of: Number,
-		required: true,
-	  },
+	participants: [{ type: String, required: true }],
+	messages: [{ type: Types.ObjectId, ref: "Message" }],
+	title: { type: String, required: true },
+	lastUpdate: { type: Date, required: true },
+	seen: { type: Map, of: Types.ObjectId },
 });
 
 const ConversationModel = mongoose.model<IConversation>("Conversation", conversationSchema);
 
 export default ConversationModel;
-
-//fqdfsdf
