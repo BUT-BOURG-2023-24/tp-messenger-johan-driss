7f9cf7ae2b02f98e0b13f8679561055f3cd9beaf
Johan <johan.gagean@gmail.com>
2023-11-15 08:46:25 +0100

    .

---
+++ src/database/Mongo/Controller/UserController.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import UserObject from "../Models/UserModel";
+
+const router = express.Router();
+
+function createUser()
+{
+    
+}
+
+function getUserByName()
+{
+
+}
+
+function getUserById()
+{
+
+}
+
+function getUsersByIds()
+{
+
+}
