eb462721b249903eb5bfa4200d833ec927771627
Johan <johan.gagean@gmail.com>
2023-11-15 09:06:48 +0100

    [ADD] - structure of Conversation and MEssage controller

---
+++ src/database/Mongo/Controller/ConversationController.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import ConversationObject from "../Models/ConversationModel";
+
+const router = express.Router();
+
+function getConversationWithParticipants()
+{
+
+}
+
+function getAllConversationsForUser()
+{
+
+}
+
+function getConversationById()
+{
+
+}
+
+function createConversation()
+{
+
+}
+
+function addMessageToConversation()
+{
+
+}
+
+function setConversationSeenForUserAndMessage()
+{
+
+}
+
+function deleteConversation()
+{
+
+}
---
+++ src/database/Mongo/Controller/MessageController.ts
@@ -0,0 +1,29 @@
+import express from "express";
+//import MessageObject from "../Models/MessageModel";
+
+const router = express.Router();
+
+function createMessage()
+{
+
+}
+
+function editMessage()
+{
+
+}
+
+function deleteMessage()
+{
+
+}
+
+function reactToMessage()
+{
+
+}
+
+function getMessageById()
+{
+
+}
