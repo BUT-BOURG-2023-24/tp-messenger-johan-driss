a94323a3ec9811013537cb20a936b2968259df1a
Johan <johan.gagean@gmail.com>
2023-11-15 17:38:45 +0100

    [ADD] - add corps package to the server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import * as http from "http";
 import express from "express";
-import { Server } from "socket.io";
-import { Database } from "./database/database";
-import { SocketController } from "./socket/socketController";
+import {Server} from "socket.io";
+import {Database} from "./database/database";
+import {SocketController} from "./socket/socketController";
+import cors from "cors";
 
 const app = express();
 
@@ -10,13 +11,19 @@ function makeApp(database: Database)
 {
 	app.locals.database = database;
 
+	app.use(cors({
+
+		credentials: true,
+
+		origin: "http://localhost:3000"
+
+	}));
+
 	const server = http.createServer(app);
 	app.use(express.json());
 
 	const io = new Server(server, { cors: { origin: "*" } });
-	let socketController = new SocketController(io, database);
-
-	app.locals.socketController = socketController;
+	app.locals.socketController = new SocketController(io, database);
 
 	return { app, server };
 }
