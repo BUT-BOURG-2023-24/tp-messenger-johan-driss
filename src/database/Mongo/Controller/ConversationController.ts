03f5aa5ebe46b3ade134a580e13787a889640c68
HMIMID DRISS p2106555 <driss.hmimid@etu.univ-lyon1.fr>
2023-11-15 11:21:43 +0100

    add method get,add,delete but need to modified after create conv service

--- src/database/Mongo/Controller/ConversationController.ts
+++ src/database/Mongo/Controller/ConversationController.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
 import ConversationModel, { IConversation } from "../Models/ConversationModel";
 import { MessageModel } from "../Models/MessageModel";
 //import * as ConversationService from 
 
+const router = express.Router();
 async function createConversation(title: string, participants: string[], message: string[], lastUpdate: Date, seen: Map<number, number>): Promise<IConversation> {
     const conversation = new ConversationModel({title, participants, message, lastUpdate, seen})
     return await conversation.save();
@@ -16,17 +17,35 @@ async function deleteConversation(idConv: number): Promise<void> {
     await ConversationModel.findById(idConv)
 }
 
-async function addMessageToConversation(req: Request, res: Response){
-    try {
-        const conversationId = req.params.conversationId;
-        const message = req.body;
-        
-        const newMessage = new MessageModel(message);
+async function addMessageToConversation(req: Request, res: Response, idConv: number): Promise<string>{
+    let conversation: IConversation;
+    conversation = getConversationById(idConv);
 
-        const updatedConversation = await createConversation(req.params.)
+    if(!conversation){
+        return "Conversation not found";
+    }else{
+        try {
+            const message = conversation.;
+            const lastUpdate = new Date(req.params.lastUpdate);
+            const seen = new Map<number, number>();
+            
+            const newMessage = new MessageModel(message);
+
+            const updatedConversation = await createConversation(req.params.title,req.params.participants,message,lastUpdate,seen)
+        }
     }
 }
 
 async function getConversationWithParticipants(req: Request, res: Response){
     
+}
+
+function getAllConversationsForUser()
+{
+
+}
+
+function setConversationSeenForUserAndMessage()
+{
+
 }
