5eaf57b4730e6d28d5891032f090291142c07c06
Johan <johan.gagean@gmail.com>
2023-12-10 12:07:58 +0100

    [UPDATE] - update all Model to fix some problems and clean code

--- src/database/Mongo/Models/UserModel.ts
+++ src/database/Mongo/Models/UserModel.ts
@@ -1,16 +1,15 @@
 import mongoose, { Schema, Document } from "mongoose";
-import { MongooseID } from "../../../types";
 
 export interface IUser extends Document {
     username: string;
     password: string;
-    profilePicId: MongooseID; // Assurez-vous que le type MongooseID est correct pour vos besoins.
+    profilePicId: string;
 }
 
 const userSchema: Schema<IUser> = new Schema<IUser>({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePicId: { type: Schema.Types.ObjectId, required: true },
+    profilePicId: { type: String, required: true },
 });
 
 const UserModel = mongoose.model<IUser>("User", userSchema);
